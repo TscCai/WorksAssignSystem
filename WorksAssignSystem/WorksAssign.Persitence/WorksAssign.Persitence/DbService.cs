fb7b61e9861fa4e66235afaf9f79fb37731ead02
TscCai <caijiran@hotmail.com>
2020-11-16 18:28:35 +0800

    增加持久化层和单元测试

---
+++ WorksAssignSystem/WorksAssign.Persitence/WorksAssign.Persitence/DbService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorksAssign.Persistence
+{
+	public class DbService
+	{
+		public static IQueryable<Employee> GetAllEmpolyees()
+		{
+			WorksAssignEntities db = new WorksAssignEntities();
+			return db.Employee;
+		}
+	}
+}
---
+++ WorksAssignSystem/WorksAssign.Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WorksAssign.Persistence;
+
+namespace WorksAssign.Test
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var e = DbService.GetAllEmpolyees();
+			foreach (var i in e)
+			{
+				Console.WriteLine(i.Name);
+			}
+		}
+	}
+}
