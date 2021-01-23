ec71e2355b3bbde4ad45c5a1a6d46f038fe59d58
TscCai <caijiran@hotmail.com>
2021-01-23 13:45:41 +0800

    增加多尺寸图标，修改窗体图标显示模式

--- WorksAssignSystem/WorksAssign/Main.Designer.cs
+++ WorksAssignSystem/WorksAssign/Main.Designer.cs
@@ -36,12 +36,13 @@
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(101F, 101F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(1063, 645);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Main";
+            this.ShowIcon = true;
             this.Text = "工作安排";
             this.ResumeLayout(false);
 
